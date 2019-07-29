32e3efb03d7da6c23472e2e3433a1a518ec86a28
emilygrobertson <53453748+emilygrobertson@users.noreply.github.com>
2019-07-29 19:53:12 -0400

    Add files via upload

---
+++ Lab 1/Lab 1/Program.cs
@@ -0,0 +1,24 @@
+﻿//Grading ID: A4945
+//CIS 199-01
+//Lab 1
+//Due: 9/2/2018
+// This is a simple lab
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Grading ID: A4945");
+            System.Console.WriteLine("Hobbies: Reading, Watching Netflix, and Hanging out with Friends");
+            System.Console.WriteLine("Favorite Book: Small Great Things by Jodi Picoult ");
+            System.Console.WriteLine("Favorite Movie: The Wolf of Wall Street");
+        }
+    }
+}

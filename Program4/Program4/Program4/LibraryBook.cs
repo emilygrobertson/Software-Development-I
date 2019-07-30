027200d1cbd4c52a70b3f4bc34b4121c7c25b810
emilygrobertson <53453748+emilygrobertson@users.noreply.github.com>
2019-07-29 20:22:28 -0400

    Add files via upload

---
+++ Program4/Program4/Program4/LibraryBook.cs
@@ -0,0 +1,111 @@
+﻿// A4945
+//Program 4
+//Due date: 12/4/2018
+//CIS 199-01
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program4
+{
+    public class LibraryBook
+    { // Setting the Backing Fields
+        private string _title;
+        private string _author;
+        private string _publisher;
+        private int _copyrightYear;
+        private string _callnumber;
+
+        //5-parameter contstructor
+        public LibraryBook (string title, string author, string publisher, int copyrightYear, string callNumber)
+        {  
+            Title = title;
+            Author = author;
+            Publisher = publisher;
+            CopyrightYear = copyrightYear;
+            CallNumber = callNumber;
+        }
+        //string property for Title
+        public string Title
+        {
+            get
+            { return _title; }
+            set
+            { _title = value; }
+        }
+        //string property for Author
+        public string Author
+        {
+            get
+            { return _author; }
+            set
+            { _author = value; }
+        }
+        //string property for publisher
+        public string Publisher
+        {
+            get
+            { return _publisher; }
+            set
+            { _publisher = value; }
+        }
+
+        //string property for copyright year
+        public int CopyrightYear
+        {
+            get
+            { return _copyrightYear; }
+            set
+            {
+                if (value >= 0)
+                    _copyrightYear = value;
+                else
+                    _copyrightYear = 2018;
+            }
+        }
+
+        //property string for call number
+        public string CallNumber
+        {
+            get
+            { return _callnumber; }
+            set
+            { _callnumber = value; }
+        }
+        bool BookCheckedOut = false;
+        //Method for book's checked out status
+        public void CheckedOut()
+        {
+            BookCheckedOut = true;
+        }
+        //Method for book's return to shelf status
+        public void ReturnToShelf()
+        {
+            BookCheckedOut = false;
+        }
+        // Precondition: Method that returns true if book is checked out and false if it isn't 
+        public bool IsCheckedOut()
+        {
+            bool isCheckedOut;
+            if (BookCheckedOut)
+            {
+                isCheckedOut = true;
+            }
+            else
+                isCheckedOut = false;
+            return isCheckedOut;
+        }
+        //Postcondition: This method will help the user know if the book is checked out
+
+        // Precondition: This method will return a string for all of the information about a book
+        public override string ToString()
+        {
+         return $"The Title is {Title}" + $"{Environment.NewLine} The Author is {Author}" + $"{Environment.NewLine} The Publisher is {Publisher}" + $"{Environment.NewLine} The Copyright Year is {CopyrightYear}" + $"{Environment.NewLine} The CallNumber is {CallNumber}" + $"{Environment.NewLine} Checked Out? {BookCheckedOut}";
+        }
+    }
+    // Postcondition: This method will show the output for the user.
+    
+}
+
---
+++ Program4/Program4/Program4/TestProgram.cs
@@ -0,0 +1,68 @@
+﻿//A4945
+//CIS 199-01
+//Due date: 12/4
+// Program 4
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program4
+{
+    class TestProgram
+    {
+        static void Main(string[] args)
+        { //Derived Class Objects
+          // Pre-Condition: The five Library Books below are the books we will use for this Program
+            LibraryBook LibraryBook1 = new LibraryBook("Twilight", "Stephenie Meyer", "Little, Brown and Company", 2005, "00001");
+            LibraryBook LibraryBook2 = new LibraryBook("New Moon", "Stephenie Meyer", "Little, Brown and Company", 2006, "00002");
+            LibraryBook LibraryBook3 = new LibraryBook("Eclipse", "Stephenie Meyer", "Little, Brown and Company", 2007,"00003");
+            LibraryBook LibraryBook4 = new LibraryBook("Breaking Dawn", "Stephenie Meyer", "Little, Brown and Company", 2008,"00004");
+            LibraryBook LibraryBook5 = new LibraryBook("To Kill a Mockingbird", "Harper Lee", "J.B. Lippincott & Co.", 1960,"00005");
+
+            // Five element Book Array
+            LibraryBook[] books = new LibraryBook[5];
+
+            //initialize array with Employees of derived types
+            books[0] = LibraryBook1;
+            books[1] = LibraryBook2;
+            books[2] = LibraryBook3;
+            books[3] = LibraryBook4;
+            books[4] = LibraryBook5;
+
+
+            foreach (LibraryBook currentBook in books)
+            {
+               Console. WriteLine(currentBook); // invokes ToString implicitly
+                Console.WriteLine();
+
+            }
+            //Postcondition: End foreach
+
+            //Precondition: Book 1, Book 2, and Book 3 were checked out of the Library
+            LibraryBook1.CheckedOut();
+            LibraryBook2.CheckedOut();
+            LibraryBook3.CheckedOut();
+            //Postconditon: none
+            
+            //Precondition: This loop prints the data to the console
+            foreach (LibraryBook currentBook in books)
+            {
+                Console.WriteLine(currentBook); // invokes ToString implicitly
+                Console.WriteLine();
+
+            }
+            //Postcondition: The library users need to return the books that were previously checked out.
+
+            //Precondition: The previously checked out books are now being returned.
+
+            LibraryBook1.ReturnToShelf();
+            LibraryBook2.ReturnToShelf();
+            LibraryBook3.ReturnToShelf();
+           //Postcondition: The system is ready for more users to check out books.
+        }
+    }
+}

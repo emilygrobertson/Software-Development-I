573e830bfee73a248c9740878b880774aa09f852
emilygrobertson <53453748+emilygrobertson@users.noreply.github.com>
2019-07-29 20:13:18 -0400

    Add files via upload

---
+++ Lab6/Lab6/Lab6/Lab6/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace Lab6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.wordsTypedTxt = new System.Windows.Forms.TextBox();
+            this.wordsLbl = new System.Windows.Forms.Label();
+            this.gradeButton = new System.Windows.Forms.Button();
+            this.outputLbl = new System.Windows.Forms.Label();
+            this.outputLabel = new System.Windows.Forms.Label();
+            this.gradeLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // wordsTypedTxt
+            // 
+            this.wordsTypedTxt.Location = new System.Drawing.Point(312, 63);
+            this.wordsTypedTxt.Name = "wordsTypedTxt";
+            this.wordsTypedTxt.Size = new System.Drawing.Size(100, 20);
+            this.wordsTypedTxt.TabIndex = 0;
+            // 
+            // wordsLbl
+            // 
+            this.wordsLbl.AutoSize = true;
+            this.wordsLbl.Location = new System.Drawing.Point(113, 66);
+            this.wordsLbl.Name = "wordsLbl";
+            this.wordsLbl.Size = new System.Drawing.Size(163, 13);
+            this.wordsLbl.TabIndex = 1;
+            this.wordsLbl.Text = "Enter the number of words typed:";
+            // 
+            // gradeButton
+            // 
+            this.gradeButton.Location = new System.Drawing.Point(204, 127);
+            this.gradeButton.Name = "gradeButton";
+            this.gradeButton.Size = new System.Drawing.Size(198, 25);
+            this.gradeButton.TabIndex = 2;
+            this.gradeButton.Text = "Click Here to Calculate your Grade!";
+            this.gradeButton.UseVisualStyleBackColor = true;
+            this.gradeButton.Click += new System.EventHandler(this.gradeButton_Click);
+            // 
+            // outputLbl
+            // 
+            this.outputLbl.AutoSize = true;
+            this.outputLbl.Location = new System.Drawing.Point(284, 187);
+            this.outputLbl.Name = "outputLbl";
+            this.outputLbl.Size = new System.Drawing.Size(0, 13);
+            this.outputLbl.TabIndex = 3;
+            // 
+            // outputLabel
+            // 
+            this.outputLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.outputLabel.Location = new System.Drawing.Point(252, 177);
+            this.outputLabel.Name = "outputLabel";
+            this.outputLabel.Size = new System.Drawing.Size(100, 23);
+            this.outputLabel.TabIndex = 4;
+            // 
+            // gradeLabel
+            // 
+            this.gradeLabel.AutoSize = true;
+            this.gradeLabel.Location = new System.Drawing.Point(174, 178);
+            this.gradeLabel.Name = "gradeLabel";
+            this.gradeLabel.Size = new System.Drawing.Size(72, 13);
+            this.gradeLabel.TabIndex = 5;
+            this.gradeLabel.Text = "Your grade is:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.gradeLabel);
+            this.Controls.Add(this.outputLabel);
+            this.Controls.Add(this.outputLbl);
+            this.Controls.Add(this.gradeButton);
+            this.Controls.Add(this.wordsLbl);
+            this.Controls.Add(this.wordsTypedTxt);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox wordsTypedTxt;
+        private System.Windows.Forms.Label wordsLbl;
+        private System.Windows.Forms.Button gradeButton;
+        private System.Windows.Forms.Label outputLbl;
+        private System.Windows.Forms.Label outputLabel;
+        private System.Windows.Forms.Label gradeLabel;
+    }
+}
+
---
+++ Lab6/Lab6/Lab6/Lab6/Form1.cs
@@ -0,0 +1,61 @@
+﻿//Grading Id:4945
+//CIS 199-01
+//Lab 6
+//Due Date: 10/28/2018
+//This lab will tell a student their grade based on the number of words they typed.
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab6
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void gradeButton_Click(object sender, EventArgs e)
+        {   int numOfWords;// user's input
+            int[] wordsTypedLowLimits = { 0, 16, 31, 51, 76 };//The lowest number of words typed in a set.
+            char [] grade = {'F','D','C','B','A'};//The grades you can recieve for the number of words typed.
+            bool found = false;
+            char studentGrade = 'F';// If a student doesn't type any words.
+            
+            int index = wordsTypedLowLimits.Length - 1;//Start from the end since they are limits.
+
+
+            //This will test the user's input with the grade they should recieve
+            if (int.TryParse(wordsTypedTxt.Text, out numOfWords))
+            {
+
+                while (index >= 0 && !found)//I used a loop to figure out which grade they will have based on the number of words typed.
+                {
+                    if (numOfWords >= wordsTypedLowLimits[index])
+                        found = true;
+                    else
+                        --index;
+                }
+                //States that the student did put a number into the textox.
+                if (found)
+                    studentGrade = grade[index];
+                outputLabel.Text = $"{studentGrade}";
+            }
+            //The user needs to input a number into the textbox
+            else
+            MessageBox.Show("Enter the number of words typed:");//This will show the user's letter grade.
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}

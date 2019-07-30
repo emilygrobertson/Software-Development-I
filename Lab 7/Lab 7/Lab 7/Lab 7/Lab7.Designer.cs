e9638751f9821be322aad5891272701a70de8a8c
emilygrobertson <53453748+emilygrobertson@users.noreply.github.com>
2019-07-29 20:11:50 -0400

    Add files via upload

---
+++ Lab 7/Lab 7/Lab 7/Lab 7/Lab7.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace Lab_7
+{
+    partial class Lab7
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.futureValueLbl = new System.Windows.Forms.Label();
+            this.interestRateLbl = new System.Windows.Forms.Label();
+            this.yearLbl = new System.Windows.Forms.Label();
+            this.presentValueLbl = new System.Windows.Forms.Label();
+            this.calculateButton = new System.Windows.Forms.Button();
+            this.futureValueTxt = new System.Windows.Forms.TextBox();
+            this.interestRateTxt = new System.Windows.Forms.TextBox();
+            this.numOfYearsTxt = new System.Windows.Forms.TextBox();
+            this.outputVLbl = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // futureValueLbl
+            // 
+            this.futureValueLbl.AutoSize = true;
+            this.futureValueLbl.Location = new System.Drawing.Point(28, 32);
+            this.futureValueLbl.Name = "futureValueLbl";
+            this.futureValueLbl.Size = new System.Drawing.Size(70, 13);
+            this.futureValueLbl.TabIndex = 0;
+            this.futureValueLbl.Text = "Future Value:";
+            // 
+            // interestRateLbl
+            // 
+            this.interestRateLbl.AutoSize = true;
+            this.interestRateLbl.Location = new System.Drawing.Point(12, 80);
+            this.interestRateLbl.Name = "interestRateLbl";
+            this.interestRateLbl.Size = new System.Drawing.Size(107, 13);
+            this.interestRateLbl.TabIndex = 1;
+            this.interestRateLbl.Text = "Annual Interest Rate:";
+            // 
+            // yearLbl
+            // 
+            this.yearLbl.AutoSize = true;
+            this.yearLbl.Location = new System.Drawing.Point(28, 134);
+            this.yearLbl.Name = "yearLbl";
+            this.yearLbl.Size = new System.Drawing.Size(82, 13);
+            this.yearLbl.TabIndex = 2;
+            this.yearLbl.Text = "No. of the Year:";
+            // 
+            // presentValueLbl
+            // 
+            this.presentValueLbl.AutoSize = true;
+            this.presentValueLbl.Location = new System.Drawing.Point(28, 192);
+            this.presentValueLbl.Name = "presentValueLbl";
+            this.presentValueLbl.Size = new System.Drawing.Size(76, 13);
+            this.presentValueLbl.TabIndex = 3;
+            this.presentValueLbl.Text = "Present Value:";
+            // 
+            // calculateButton
+            // 
+            this.calculateButton.Location = new System.Drawing.Point(146, 237);
+            this.calculateButton.Name = "calculateButton";
+            this.calculateButton.Size = new System.Drawing.Size(75, 23);
+            this.calculateButton.TabIndex = 4;
+            this.calculateButton.Text = "Calculate!";
+            this.calculateButton.UseVisualStyleBackColor = true;
+            this.calculateButton.Click += new System.EventHandler(this.calculateButton_Click);
+            // 
+            // futureValueTxt
+            // 
+            this.futureValueTxt.Location = new System.Drawing.Point(136, 29);
+            this.futureValueTxt.Name = "futureValueTxt";
+            this.futureValueTxt.Size = new System.Drawing.Size(100, 20);
+            this.futureValueTxt.TabIndex = 5;
+            // 
+            // interestRateTxt
+            // 
+            this.interestRateTxt.Location = new System.Drawing.Point(136, 72);
+            this.interestRateTxt.Name = "interestRateTxt";
+            this.interestRateTxt.Size = new System.Drawing.Size(100, 20);
+            this.interestRateTxt.TabIndex = 6;
+            // 
+            // numOfYearsTxt
+            // 
+            this.numOfYearsTxt.Location = new System.Drawing.Point(136, 126);
+            this.numOfYearsTxt.Name = "numOfYearsTxt";
+            this.numOfYearsTxt.Size = new System.Drawing.Size(100, 20);
+            this.numOfYearsTxt.TabIndex = 7;
+            // 
+            // outputVLbl
+            // 
+            this.outputVLbl.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.outputVLbl.Location = new System.Drawing.Point(136, 182);
+            this.outputVLbl.Name = "outputVLbl";
+            this.outputVLbl.Size = new System.Drawing.Size(100, 23);
+            this.outputVLbl.TabIndex = 8;
+            // 
+            // Lab7
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(373, 286);
+            this.Controls.Add(this.outputVLbl);
+            this.Controls.Add(this.numOfYearsTxt);
+            this.Controls.Add(this.interestRateTxt);
+            this.Controls.Add(this.futureValueTxt);
+            this.Controls.Add(this.calculateButton);
+            this.Controls.Add(this.presentValueLbl);
+            this.Controls.Add(this.yearLbl);
+            this.Controls.Add(this.interestRateLbl);
+            this.Controls.Add(this.futureValueLbl);
+            this.Name = "Lab7";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label futureValueLbl;
+        private System.Windows.Forms.Label interestRateLbl;
+        public System.Windows.Forms.Label yearLbl;
+        private System.Windows.Forms.Label presentValueLbl;
+        private System.Windows.Forms.Button calculateButton;
+        private System.Windows.Forms.TextBox futureValueTxt;
+        private System.Windows.Forms.TextBox interestRateTxt;
+        private System.Windows.Forms.TextBox numOfYearsTxt;
+        private System.Windows.Forms.Label outputVLbl;
+    }
+}
+
---
+++ Lab 7/Lab 7/Lab 7/Lab 7/Lab7.cs
@@ -0,0 +1,51 @@
+﻿//Grading ID: A4945
+//Due: 11/11/2018
+//CIS 199-01
+//This lab is intended to calculate how much money you need to invest today to earn a certain amount in the future. 
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab_7
+{
+    public partial class Lab7 : Form
+    {
+        public Lab7()
+        {
+            InitializeComponent();
+        }
+        //Predondition: The user MUST enter a value in each box or else their caluclation will not work properly.
+        //This method uses the infoprmation in the previous method to ouput the present value for the user.
+        private void calculateButton_Click(object sender, EventArgs e)
+        {
+            double futurevalue;// The future value
+            double interestrate;// The annual interest rate
+            int numofyears;//The number of years
+
+            //This will recongize the user's input from the textbox.
+            double.TryParse(futureValueTxt.Text, out futurevalue);
+            double.TryParse(interestRateTxt.Text, out interestrate);
+            int.TryParse(numOfYearsTxt.Text, out numofyears);
+
+            //This calculates and displays the present value for the user.
+            outputVLbl.Text= $"{CalcPresentValue(futurevalue, interestrate, numofyears):C}";
+
+        }
+        //This method calculates the present value then sends it to the previous method.
+        public static double CalcPresentValue(double futurevalue, double interestrate, int numofyears)
+        {
+            double presentValue;
+            presentValue = futurevalue / (Math.Pow(1 + interestrate, numofyears));
+            return presentValue;
+        }
+    }
+    
+    
+}
+ 
